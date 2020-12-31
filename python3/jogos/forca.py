48b385231a71afc3e81856a934fda6b602a35be8
Daninot <daninnot@gmail.com>
2020-12-30 21:53:05 -0300

    última atualização da forca

--- python3/jogos/forca.py
+++ python3/jogos/forca.py
@@ -5,6 +5,30 @@ def jogar():
     print("*********Jogo da forca!**********")
     print("*********************************")
 
+    palavra_secreta = "python"
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou): #palavras chaves sempre em minúsculo
+
+        chute = input("Qual letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+                #print("Encontrei a letra {} na posição {}".format(letra,index))
+            index++
+
+        print(letras_acertadas)
+
+
+    print("Fim do jogo")
 
 if(__name__ == "__main__"):
     jogar()
